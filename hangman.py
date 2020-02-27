db86069d38c40fc2ea776c8b749b8e4c92b75ad0
mbila22 <60894863+mbila22@users.noreply.github.com>
2020-02-27 13:39:38 -0500

    hi

--- hangman.py
+++ hangman.py
@@ -58,3 +58,35 @@ def check():
     
 time.sleep(5)
 hamza.clear()
+
+lives = 6
+word = ""
+letter = ""
+updatedSpaces= []
+
+def intialize ():
+    global word
+    global updatedSpaces
+    word = "airport"
+    updatedSpaces = "_ _ _ _ _ _ _"
+    print("Try to guess the word in 6 tries")
+    print(updatedSpaces)
+
+def getLetter():
+    global letter
+    print("What is your first guess?")
+    letter = raw_input()
+    
+
+def won():
+    global lives 
+    if updatedSpaces == word:
+        print("Hurray, you got the word!")
+    else:
+       'lives = lives -1
+       if lives == 0:
+           print('You lost:(')n
+def main():
+    intialize()
+    getLetter()
+    #check()
